28b271e73151499209bcee6dd45952395a2c8317
Maksim Stsiapanchanka <cumatuk@gmail.com>
2020-04-13 15:47:34 +0300

    2105

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task21.task2105;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+Исправить ошибку. Сравнение объектов
+*/
+public class Solution {
+    private final String first, last;
+
+    public Solution(String first, String last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public boolean equals(Object o) {
+        if (!(o instanceof Solution))
+            return false;
+        if (o == this)
+            return true;
+        Solution n = (Solution) o;
+        if (((n.first != null && n.first.equals(first)) && (n.last != null && n.last.equals(last)))
+                || ((n.first == null && first == null) &&(n.last == null && last == null))
+                || ((n.first != null && n.first.equals(first)) &&(n.last == null && last == null))
+                || ((n.first == null && first == null) && (n.last != null && n.last.equals(last))))
+            return true;
+        return false;
+    }
+    public int hashCode() {
+        return 31 * (first == null? 0 : first.hashCode()) + (last == null ? 0 :last.hashCode());
+    }
+
+    public static void main(String[] args) {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Mickey", "Mouse"));
+        System.out.println(s.contains(new Solution("Mickey", "Mouse")));
+    }
+}
