6ad31a88a7dec89e1f0018fc0877633526527bdc
Maksim Stsiapanchanka <cumatuk@gmail.com>
2020-04-13 17:26:56 +0300

    Multithreading

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2108/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task21.task2108;
+
+/* 
+Клонирование растений
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Tree tree = new Tree("willow", new String[]{"s1", "s2", "s3", "s4"});
+        Tree clone = null;
+        try {
+            clone = (Tree)tree.clone();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(tree);
+        System.out.println(clone);
+
+        System.out.println(tree.branches);
+        System.out.println(clone.branches);
+    }
+
+    public static class Plant {
+        private String name;
+
+        public Plant(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static class Tree extends Plant implements Cloneable {
+        private String[] branches;
+
+        public Tree(String name, String[] branches) {
+            super(name);
+            this.branches = branches;
+        }
+
+        public String[] getBranches() {
+            return branches;
+        }
+        public Object clone(){
+            String[] newArr = new String[this.branches.length];
+            for (int i = 0; i < this.branches.length; i++) {
+                newArr[i] = this.branches[i];
+            }
+            Tree newTree = new Tree(this.getName(), newArr);
+            return newTree;
+        }
+    }
+}
