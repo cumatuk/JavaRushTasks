fd5f781bc7f2418c57da2ac36b6a08a3428cfa51
Maksim Stsiapanchanka <cumatuk@gmail.com>
2020-04-17 17:42:22 +0300

    2312(8)

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Snake {
@@ -21,5 +22,23 @@ public class Snake {
 
     public SnakeDirection getDirection() {
         return direction;
+    }
+
+    public Snake(int x, int y){
+        sections = new ArrayList<>();
+        sections.add(new SnakeSection(x, y));
+        isAlive = true;
+    }
+
+    public int getX(){
+        return sections.get(0).getX();
+    }
+
+    public int getY(){
+        return sections.get(0).getY();
+    }
+
+    public void move(){
+
     }
 }
