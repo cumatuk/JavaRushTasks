f41e2b78ae77a84c84ec6992a557fc2bab157a09
Maksim Stsiapanchanka <cumatuk@gmail.com>
2020-04-17 17:19:41 +0300

    2312(4)

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Mouse.java
@@ -2,4 +2,17 @@ package com.javarush.task.task23.task2312;
 
 public class Mouse {
     private int x, y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
